5aee3816baae5dd412b0a84181618439ffd810dd
VuBui217 <vubui217@gmail.com>
2025-07-28 23:57:46 -0400

    Initial Commit

---
+++ app/companions/new/page.tsx
@@ -0,0 +1,14 @@
+
+/**
+ * This page to build a new companion
+ * @returns 
+ */
+const NewCompanion = () => {
+  return (
+    <div>
+      NewCompanion
+    </div>
+  )
+}
+
+export default NewCompanion
---
+++ app/companions/page.tsx
@@ -0,0 +1,13 @@
+/**
+ * 
+ * @returns This page is companions library
+ */
+const CompanionsLibrary = () => {
+  return (
+    <div>
+      CompanionsLibrary
+    </div>
+  )
+}
+
+export default CompanionsLibrary
